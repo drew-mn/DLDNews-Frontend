c7686cb89bda08a5484dcd623040c6ecbb7d5678
Leah Meromy <lmeromy@gmail.com>
2018-12-10 21:11:36 +0000

    Added read time to article thumbnails

--- src/components/ArticleThumb.js
+++ src/components/ArticleThumb.js
@@ -5,11 +5,20 @@ const ArticleThumb = (props) => {
 
   const {title, media, summary, id} = props.articleThumb;
 
+// assumes average adult reading speed is 250 words per minute
+  function calcReadTime(){
+    const text = props.articleThumb.fullText;
+    const wordCount = text.split(' ').length;
+    const readTime = wordCount / 250;
+    return readTime;
+  }
+
   return (
     <div>
       <Link to={'articles/' + id}
       className='headline'> {media}</Link>
       <h4>{title}</h4>
+      <h5>{calcReadTime()} min read </h5>
       <p>{summary}</p>
     </div>
   )
