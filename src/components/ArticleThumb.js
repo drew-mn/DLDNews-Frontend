991c6f1afdd1d0e0352da4dd4ee7c5e4813784a5
Leah Meromy <lmeromy@gmail.com>
2018-12-11 21:50:22 +0000

    Not sure where to go from here

--- src/components/ArticleThumb.js
+++ src/components/ArticleThumb.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 const ArticleThumb = (props) => {
 
-  const {title, media, summary, id} = props.articleThumb;
+  const {title, media, summary, readCounter, id} = props.articleThumb;
 
 // assumes average adult reading speed is 250 words per minute
   function calcReadTime(){
@@ -13,10 +13,21 @@ const ArticleThumb = (props) => {
     return readTime;
   }
 
+  function addReadCounter(){
+      readCounter += 1;
+      props.handleReadCounterAdd(props.articleThumb)
+    }
+
+// must send a put request every time this thumbnail link is clicked. The put request will simply add 1 to the readCounter.
+// How the hell can we do it?
+
+
+
+
   return (
     <div class-name="article-thumb">
       <Link to={'articles/' + id}
-      className='headline'><img src={media} alt={title}/></Link>
+      className='headline'><img src={media} alt={title} onClick={addReadCounter}/></Link>
       <h4>{title}</h4>
       <p>{summary}</p>
       <h5>{calcReadTime()} min read </h5>
