0887b2f4c34b86dbddd3345a94aeeab1e2a7782f
David Telfer <davietelfer@hotmail.com>
2018-12-11 14:25:51 +0000

    fixed issue where headline images were static whilst the summary/text etc were dynamic

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import Slider from 'react-slick';
 
-import politics1 from '../../resources/images/politics1.jpg';
-import science1 from '../../resources/images/science1.jpg';
-import tech1 from '../../resources/images/tech1.jpg';
-import business1 from '../../resources/images/business1.jpg';
-import other1 from '../../resources/images/other1.jpg';
-
 const Featured = (props) => {
 
   const display = props.articles.map((story) => {
     return <div className = "feature-text" key={story.id}>
+      <img src={story.media} alt={story.title}/>
       <h2>{story.title}</h2>
       <p>{story.summary}</p>
     </div>
@@ -38,62 +33,22 @@ const Featured = (props) => {
 
       <Slider {...settings}>
         <div>
-          <div
-            className="slider_image"
-            style={{
-              background: `url(${politics1})`,
-              height: '330px'
-            }}
-          >
-          </div>
           {display[0]}
         </div>
 
         <div>
-          <div
-            className="slider_image"
-            style={{
-              background:`url(${science1})`,
-              height: '330px'
-            }}
-          >
-          </div>
           {display[5]}
         </div>
 
         <div>
-          <div
-            className="slider_image"
-            style={{
-              background:`url(${tech1})`,
-              height: '330px'
-            }}
-          >
-          </div>
           {display[10]}
         </div>
 
         <div>
-          <div
-            className="slider_image"
-            style={{
-              background:`url(${business1})`,
-              height: '330px'
-            }}
-          >
-          </div>
           {display[15]}
         </div>
 
         <div>
-          <div
-            className="slider_image"
-            style={{
-              background:`url(${other1})`,
-              height: '330px'
-            }}
-          >
-          </div>
           {display[20]}
         </div>
 
